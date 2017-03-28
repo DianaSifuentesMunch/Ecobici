e445053fb0536b5fc7040048c5e5aef1bc601da9
DianaSifuentesMunch <diana.sifuentes-munch@polytechnique.edu>
2017-03-27 18:14:05 -0600

    Update Ecob2

--- Ecob2.R
+++ Ecob2.R
@@ -1,4 +1,3 @@
-
 #Segunda Parte
 
 #analisis de datos de 2016.
@@ -8,14 +7,12 @@ distancias<-fread("distancias.csv")
 library(data.table)
 
 #setwd
-setwd("/Users/Diana/Documents/Personal/Personal/LabCDMX/Ecobici/Ecobici")
+#setwd("/Users/Diana/Documents/Personal/Personal/LabCDMX/Ecobici/Ecobici")
 
 #leer los datos
-
 data<-fread(paste("ecobici",year,"_C.csv",sep=""),verbose=FALSE,showProgress=FALSE)
 
 #vamos a ver cuántos viajes hay de origen a destino, necesitamos una matriz.
-
 subd<-cbind(data$Ciclo_Estacion_Arribo,data$Ciclo_Estacion_Retiro)
 subd<-as.data.frame(subd)
 names(subd)<-c("Ciclo_Estacion_Arribo","Ciclo_Estacion_Retiro")
@@ -36,36 +33,68 @@ adj<-matrix(NA,nrow=dim(distancias)[1],ncol=dim(distancias)[2])
 #la llenamos
 for(i in 1:dim(aristas)[1])
 {
-s<-aristas[i,1]
-t<-aristas[i,2]
-w<-aristas[i,3]
-adj[s,t]<-w
+  s<-aristas[i,1]
+  t<-aristas[i,2]
+  w<-aristas[i,3]
+  adj[s,t]<-w
 }
 
 #para ver que tan vacía o llena esta la matriz se puede usar:
 image(1:dim(distancias)[2],1:dim(distancias),adj)
 #se puede ver que la matriz esta practicamente vacía. a partir de las estaciones xxx (approx)
 
-View(adj)
-#plot de grafica con edges por peso
 
-g<-graph_from_adjacency_matrix(adj, mode = "directed", weighted = TRUE, diag = TRUE, add.colnames = NULL, add.rownames = NA)
+#guardar el archivo
+fwrite(aristas,file=paste("OrigenDestinoLista_",year,".csv",sep=""),row.names=FALSE)
+adj<-as.data.frame(adj)
+fwrite(adj,file=paste("OrigenDestinoMatriz_",year,".csv",sep=""),row.names=FALSE)
+
+#leer cves delegaciones y colonias 
+cves<-fread("cvesColoniasEstaciones.csv")
+#matriz origen destino colonias 
+cveCol<-fread("CveEstacion_Col.csv")
+cveCol<-cveCol[order(cveCol[,1]),]
+cveCol<-as.data.frame(cveCol)
+#hacemos una matriz de adyacencia para colonias y para delegaciones
+adjCol<-matrix(0,nrow=dim(cves)[1],ncol=dim(cves)[1])
+adjDel<-matrix(0,nrow=length(unique(cves$Cve_Del)),ncol=length(unique(cves$Cve_Del)))
 
 
+aristas<-fread(paste("OrigenDestinoLista_",year,".csv",sep=""))
+
+i<-1
+for(i in 1:dim(aristas)[1])
+{
+  s<-as.numeric(aristas[i,1])
+  cols<-cveCol[s,2]
+  aux<-which(cves$Cols==cols)
+  cvecols<-as.numeric(cves[aux,2])
+  cveDels<-as.numeric(cves[aux,4])
+  
+  t<-as.numeric(aristas[i,2])
+  colt<-cveCol[t,2]
+  aux2<-which(cves$Cols==colt)
+  cvecolt<-as.numeric(cves[aux2,2])
+  cveDelt<-as.numeric(cves[aux2,4])
+  
+  w<-as.numeric(aristas[i,3])
+  aux<-as.numeric(adjCol[cvecols,cvecolt])
+  adjCol[cvecols,cvecolt]<- aux + w
+  aux2<-as.numeric(adjDel[cveDels,cveDelt])
+  adjDel[cveDels,cveDelt]<- aux2 + w
+}
+
+adjDel<-as.data.frame(adjDel)
+adjCol<-as.data.frame(adjCol)
+
+fwrite(adjDel,file=paste("OD_Delegacion",year,".csv",sep=""))
+fwrite(adjCol,file=paste("OD_Colonia",year,".csv",sep=""))
 
 
-
-
-
-
-
-
+image(1:dim(adjDel)[2],1:dim(adjDel)[2],as.matrix(adjDel))
+image(1:dim(adjCol)[2],1:dim(adjCol)[2],as.matrix(adjCol)
 
 #estadistica descriptiva
 #identificar plots principales
-#agregar el calculo con colonia o delegacion y con clima
 
-
-#guardar el archivo
-fwrite(aristas,file=paste("Aristas_",year,".csv",sep=""),row.names=FALSE)
 
