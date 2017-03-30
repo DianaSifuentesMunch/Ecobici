761e605c2ae6503cb7c6e7c68778f873a5809543
DianaSifuentesMunch <diana.sifuentes-munch@polytechnique.edu>
2017-03-30 17:16:35 +0000

    Add files via upload

--- Ecob2.R
+++ Ecob2.R
@@ -12,89 +12,129 @@ library(data.table)
 #leer los datos
 data<-fread(paste("ecobici",year,"_C.csv",sep=""),verbose=FALSE,showProgress=FALSE)
 
-#vamos a ver cuántos viajes hay de origen a destino, necesitamos una matriz.
-subd<-cbind(data$Ciclo_Estacion_Arribo,data$Ciclo_Estacion_Retiro)
-subd<-as.data.frame(subd)
-names(subd)<-c("Ciclo_Estacion_Arribo","Ciclo_Estacion_Retiro")
-d2<-melt(subd,id.vars=c(1,2),na.rm=FALSE)
-
-aristas<-dcast(d2, Ciclo_Estacion_Retiro + Ciclo_Estacion_Arribo ~ .)
-
-#comprobar si si es lo que queremos
-head(aristas)
-ind1<-which(data$Ciclo_Estacion_Retiro==1)
-ind2<-which(data$Ciclo_Estacion_Arribo==1)
-length(intersect(ind1,ind2))
 
 
-#hacemos una matriz de adyacencia con los pesos que encontramos
-adj<-matrix(NA,nrow=dim(distancias)[1],ncol=dim(distancias)[2])
+#vamos a ver cu??ntos viajes hay de origen a destino, necesitamos una lista para hacer la grafica.
+# subd<-cbind(data$Ciclo_Estacion_Arribo,data$Ciclo_Estacion_Retiro)
+# subd<-as.data.frame(subd)
+# names(subd)<-c("Ciclo_Estacion_Arribo","Ciclo_Estacion_Retiro")
+# d2<-melt(subd,id.vars=c(1,2),na.rm=FALSE)
+# aristas<-dcast(d2, Ciclo_Estacion_Retiro + Ciclo_Estacion_Arribo ~ .)
+# #comprobar si si es lo que queremos
+# head(aristas)
+# ind1<-which(data$Ciclo_Estacion_Retiro==1)
+# ind2<-which(data$Ciclo_Estacion_Arribo==1)
+# length(intersect(ind1,ind2))
+# fwrite(aristas,file=paste("OrigenDestinoLista_",year,".csv",sep=""),row.names=FALSE)
 
-#la llenamos
-for(i in 1:dim(aristas)[1])
-{
-  s<-aristas[i,1]
-  t<-aristas[i,2]
-  w<-aristas[i,3]
-  adj[s,t]<-w
-}
+#
+OD<-table(data$Ciclo_Estacion_Retiro,data$Ciclo_Estacion_Arribo)
+#OD[1:10,1:10]
+write.table(OD,file=paste("OrigenDestinoMatriz_",year,".csv",sep=""),sep=",")
 
-#para ver que tan vacía o llena esta la matriz se puede usar:
-image(1:dim(distancias)[2],1:dim(distancias),adj)
-#se puede ver que la matriz esta practicamente vacía. a partir de las estaciones xxx (approx)
+OD2<-as.data.frame(OD)
+#OD2[c(1:5,456:460),]
+names(OD2)<-c("source","target","weight")
+fwrite(OD2,file=paste("OrigenDestinoLista_",year,".csv",sep=""),row.names=FALSE)
 
+#matriz de adyacencia con pesos para previsualizacion
+adj<-matrix(OD2$weight,nrow=dim(distancias)[1],ncol=dim(distancias)[2],byrow=TRUE)
 
+deciles<-quantile(as.numeric(adj),probs=seq(0,1,.05),na.rm=FALSE)
+deciles
+unique(deciles)
+
+#para ver que tan vac??a o llena esta la matriz se puede usar:
+#image(1:dim(distancias)[2],1:dim(distancias),log(adj)) #En matlab heatmap es mejor
+#se puede ver donde tiene huecos la matriz.
 #guardar el archivo
-fwrite(aristas,file=paste("OrigenDestinoLista_",year,".csv",sep=""),row.names=FALSE)
-adj<-as.data.frame(adj)
-fwrite(adj,file=paste("OrigenDestinoMatriz_",year,".csv",sep=""),row.names=FALSE)
 
-#leer cves delegaciones y colonias 
+#leer cves delegaciones y colonias
 cves<-fread("cvesColoniasEstaciones.csv")
-#matriz origen destino colonias 
+#matriz origen destino colonias
 cveCol<-fread("CveEstacion_Col.csv")
 cveCol<-cveCol[order(cveCol[,1]),]
 cveCol<-as.data.frame(cveCol)
+
 #hacemos una matriz de adyacencia para colonias y para delegaciones
-adjCol<-matrix(0,nrow=dim(cves)[1],ncol=dim(cves)[1])
-adjDel<-matrix(0,nrow=length(unique(cves$Cve_Del)),ncol=length(unique(cves$Cve_Del)))
+class(data$Ciclo_Estacion_Retiro)
+
+colSource<-cveCol[data$Ciclo_Estacion_Retiro,2]
+colDestino<-cveCol[data$Ciclo_Estacion_Arribo,2]
+
+ODCol<-table(colSource,colDestino)
+write.table(ODCol,file=paste("OrigenDestinoColoniasMatriz_",year,".csv",sep=""),sep=",")
+ODCol2<-as.data.frame(ODCol)
+#OD2[c(1:5,456:460),]
+names(ODCol2)<-c("source","target","weight")
+fwrite(ODCol2,file=paste("OrigenDestinoColoniasLista_",year,".csv",sep=""),row.names=FALSE)
 
 
-aristas<-fread(paste("OrigenDestinoLista_",year,".csv",sep=""))
+#hacemos una matriz de adyacencia para delegaciones
+DelSource<-match( colSource, cves$Cols)
+DelDestino<-match( colDestino, cves$Cols)
+DelSource<-cves$Del[DelSource]
+DelDestino<-cves$Del[DelDestino]
+ODDel<-table(DelSource,DelDestino)
 
-i<-1
-for(i in 1:dim(aristas)[1])
-{
-  s<-as.numeric(aristas[i,1])
-  cols<-cveCol[s,2]
-  aux<-which(cves$Cols==cols)
-  cvecols<-as.numeric(cves[aux,2])
-  cveDels<-as.numeric(cves[aux,4])
-  
-  t<-as.numeric(aristas[i,2])
-  colt<-cveCol[t,2]
-  aux2<-which(cves$Cols==colt)
-  cvecolt<-as.numeric(cves[aux2,2])
-  cveDelt<-as.numeric(cves[aux2,4])
-  
-  w<-as.numeric(aristas[i,3])
-  aux<-as.numeric(adjCol[cvecols,cvecolt])
-  adjCol[cvecols,cvecolt]<- aux + w
-  aux2<-as.numeric(adjDel[cveDels,cveDelt])
-  adjDel[cveDels,cveDelt]<- aux2 + w
-}
+write.table(ODDel,file=paste("OrigenDestinoDelsMatriz_",year,".csv",sep=""),sep=",")
 
-adjDel<-as.data.frame(adjDel)
-adjCol<-as.data.frame(adjCol)
+ODDel2<-as.data.frame(ODDel)
+#OD2[c(1:5,456:460),]
+names(ODDel2)<-c("source","target","weight")
+fwrite(ODDel2,file=paste("OrigenDestinoDelsLista_",year,".csv",sep=""),row.names=FALSE)
 
-fwrite(adjDel,file=paste("OD_Delegacion",year,".csv",sep=""))
-fwrite(adjCol,file=paste("OD_Colonia",year,".csv",sep=""))
-
-
-image(1:dim(adjDel)[2],1:dim(adjDel)[2],as.matrix(adjDel))
-image(1:dim(adjCol)[2],1:dim(adjCol)[2],as.matrix(adjCol)
 
 #estadistica descriptiva
-#identificar plots principales
+
+#distancia por viaje promedio
+mean(data$distancia[which(data$distancia!=0)])
+#1653.927 km
 
 
+#día de la semana de uso de bici
+sem<-table(data$dia_semana)
+sem/sum(sem)
+
+#   Friday     Monday   Saturday     Sunday   Thursday    Tuesday  Wednesday
+#0.16522777 0.16557302 0.07603575 0.06314005 0.17625761 0.18038718 0.17337861
+
+
+
+#dia de la semana de uso de bici por sexo
+semsex1<-table(data$dia_semana,names=data$Genero_Usuario)
+
+
+semsex<-cbind(semsex1[,1]/sum(semsex1[,1]),semsex1[,2]/sum(semsex1[,2]))
+colnames(semsex)<-c("F","M")
+semsex<-semsex[c(2,6,7,5,1,3,4),]
+semsex1<-semsex1[c(2,6,7,5,1,3,4),]
+
+
+
+#semsex
+#                   F          M
+#Friday    0.15933257 0.16718332
+#Monday    0.16248714 0.16659667
+#Saturday  0.08317323 0.07366811
+#Sunday    0.07606373 0.05885302
+#Thursday  0.17187470 0.17771151
+#Tuesday   0.17716477 0.18145611
+#Wednesday 0.16990387 0.17453125
+
+
+#plot(semsex)
+
+options(digits = 2)
+#uso por hora vs sexo
+hrsex1<-table(data$Hora_T,names=data$Genero_Usuario)
+hrsex<-cbind(hrsex1[,1]/sum(hrsex1[,1]),hrsex1[,2]/sum(hrsex1[,2]))
+colnames(hrsex)<-c("F","M")
+print(hrsex*100)
+
+
+#Multas:
+
+multas<-length(which(data$duracion>45))
+multas/dim(data)[1]
+.29%
--- Ecobici.R
+++ Ecobici.R
@@ -1,49 +1,4 @@
 {
-# t1<-c()
-# t2<-c()
-#
-# sec<-seq(1,dim(data)[1],50000)
-# for(i in 1:length(sec))
-# {
-# if(i == length(sec)){
-# 										inf<-sec[i]
-# 										sup<-dim(data)[1]
-# }else{
-# 			inf<-sec[i]
-# 			sup<-sec[(i+1)]-1
-# 			}
-#
-# aux<-seq(inf,sup,1)
-# aux2<-unlist(lapply(aux,function(x) paste(data[x,5],data[x,6],sep=" ")))
-# aux2<-substr(aux2,1,19)
-# aux2<-strptime(aux2,format="%Y-%m-%d %H:%M:%S",tz="")
-# t1<-c(t1,aux2)
-# aux3<-unlist(lapply(aux,function(x) paste(data[x,8],data[x,9],sep=" ")))
-# aux3<-substr(aux3,1,19)
-# aux3<-strptime(aux3,format="%Y-%m-%d %H:%M:%S",tz="")
-# t2<-c(t2,aux3)
-# print(i)
-# }
-# aux<-format(data$Hora_Retiro,'%H')
-# data<-cbind(data,aux)
-# names(data)[length(names(data))]<-"Hora_T"
-# data$Hora_T<-as.numeric(levels(data$Hora_T))[data$Hora_T]
-# #mes
-# aux<-format(data$Hora_Retiro,'%M')
-# data<-cbind(data,aux)
-# names(data)[length(names(data))]<-"Mes"
-# class(data$Mes)
-# #data$Mes<-as.numeric(levels(data$Hora_T))[data$Hora_T]
-# #año
-# aux<-format(data$Hora_Retiro,'%Y')
-# data<-cbind(data,aux)
-# names(data)[length(names(data))]<-"Year"
-# class(data$Year)
-# data$Year<-as.numeric(levels(data$Year))[data$Year]
-
-	}
-
-{
   # t1<-c()
   # t2<-c()
   #
@@ -79,28 +34,77 @@
   # names(data)[length(names(data))]<-"Mes"
   # class(data$Mes)
   # #data$Mes<-as.numeric(levels(data$Hora_T))[data$Hora_T]
-  # #año
+  # #a??o
   # aux<-format(data$Hora_Retiro,'%Y')
   # data<-cbind(data,aux)
   # names(data)[length(names(data))]<-"Year"
   # class(data$Year)
   # data$Year<-as.numeric(levels(data$Year))[data$Year]
-  
+
 }
-
-rm(list=ls(all=TRUE))
-year<-2016
-
-
-#preguntas:
-#cual es la distancia lineal promedio?
-data<-fread(paste("ecobici",year,"_C.csv",sep=""))
-mean(data$distancia[-which(data$distancia==0)])
-
-
-names(data)
-#que dia de la semana se usa más la bici?
-barplot(table(data$dia_semana))
+{
+# horas<-as.POSIXct()
+#
+#
+# strptime_C<-cmpfun(strptime)
+# difftime_C<-cmpfun(difftime)
+# as.ITime_C<-cmpfun(as.ITime)
+#
+#
+# strp<-function(horas,horas2)
+# {
+# aux3<-strptime(horas,format="%H:%M:%S",tz="")
+# aux4<-strptime(horas2,format="%H:%M:%S",tz="")
+# interval<-difftime(aux4,aux3)
+# return(list(aux3,aux4,interval))
+# }
+#
+# strp2<-function(horas,horas2)
+# {
+#   aux3<-strptime_C(horas,format="%H:%M:%S",tz="")
+#   aux4<-strptime_C(horas2,format="%H:%M:%S",tz="")
+#   interval<-difftime_C(aux4,aux3)
+#   return(list(aux3,aux4,interval))
+# }
+#
+# Id<-function(horas,horas2)
+# {
+#   aux3p<-as.ITime(horas)
+#   aux4p<-as.ITime(horas2)
+#   interval2<-as.integer(aux4p)-as.integer(aux3p)
+#   return(list(aux3p,aux4p,interval2))
+# }
+#
+# Id2<-function(horas,horas2)
+# {
+#   aux3p<-as.ITime_C(horas)
+#   aux4p<-as.ITime_C(horas2)
+#   interval2<-as.integer(aux4p)-as.integer(aux3p)
+#   return(list(aux3p,aux4p,interval2))
+# }
+#
+# strp2_C<-cmpfun(strp2)
+# Id2_C<-cmpfun(Id2)
+#
+# #system.time(strp(horas,horas2))
+# #system.time(Id(horas,horas2))
+# #user  system elapsed
+# #217.580   1.554 220.682
+# #> system.time(Id(horas,horas2))
+# #user  system elapsed
+# #216.324   0.892 218.333
+# #system.time(strp2_C(horas,horas2))
+# #system.time(Id2_C(horas,horas2))
+# #user  system elapsed
+# #213.434   1.450 216.225
+# #> system.time(Id2_C(horas,horas2))
+# #user  system elapsed
+# #215.696   0.974 217.729
+#
+# system.time()
+#
+# aux4<-strptime(horas2,format="%H:%M:%S",tz="")
+}
 
 
 install.packages("data.table")
@@ -110,6 +114,7 @@ install.packages("sp")
 install.packages("geosphere")
 install.packages("curl")
 install.packages("parallel")
+install.packages("fasttime")
 rm(list=ls(all=TRUE))
 
 
@@ -119,39 +124,50 @@ library(data.table)
 library(geosphere)
 library(curl)
 library(parallel)
+library(fasttime)
+
+#strptime_C<-cmpfun(strptime)
+#difftime_C<-cmpfun(difftime)
+#Idate_C<-cmpfun(as.IDate)
 
 
 #set working directory
 setwd("/Users/JoseManuel/Documents/DianaMunch")
 
-###Leer los datos de las localizaciones de las estaciones para hacer un cálculo de la distancia de los recorridos recorrida
+
+
+###Leer los datos de las localizaciones de las estaciones para hacer un c??lculo de la distancia de los recorridos recorrida
 ###calcular distancias
+{
 
-data<-read.table(file="estaciones.csv", header = TRUE, sep = ",", quote = "\"'", dec = ".", as.is = TRUE)
+  data<-read.table(file="estaciones.csv", header = TRUE, sep = ",", quote = "\"'", dec = ".", as.is = TRUE)
 
-index<-order(data[,1])
-data<-data[index,]
-data<-cbind(data$id,data$location.lat,data$location.lon,data$nearbyStations.0,data$nearbyStations.1,data$nearbyStations.2,data$nearbyStations.3)
-data<-as.data.frame(data)
-names(data)<-c("id","lat","lon","C1","C2","C3","C4")
+  index<-order(data[,1])
+  data<-data[index,]
+  data<-cbind(data$id,data$location.lat,data$location.lon,data$nearbyStations.0,data$nearbyStations.1,data$nearbyStations.2,data$nearbyStations.3)
+  data<-as.data.frame(data)
+  names(data)<-c("id","lat","lon","C1","C2","C3","C4")
 
-distancias<-distm(cbind(data$lon,data$lat),cbind(data$lon,data$lat),fun=distHaversine)
+  distancias<-distm(cbind(data$lon,data$lat),cbind(data$lon,data$lat),fun=distHaversine)
 
-distancias<-fread("distancias.csv")
-distancias<-distancias[,-1]
-fwrite(distancias,file="distancias.csv",col.names=FALSE)
+  distancias<-fread("distancias.csv")
+  distancias<-distancias[,-1]
+  fwrite(distancias,file="distancias.csv",col.names=FALSE)
+}
+
+
+
 
 #leer varios conjuntos de datos:
-#hacer una lista de las urls donde estan los datos separadas por comas y entre comillas. También se puede leer un file en donde esten todas las direcciones. la instrucción para concatenar un vector es c(,,..,)
+#hacer una lista de las urls donde estan los datos separadas por comas y entre comillas. Tambi??n se puede leer un file en donde esten todas las direcciones. la instrucci??n para concatenar un vector es c(,,..,)
 #hay datos desde feb 2010 hasta febrero 2017.
 meses<-c("01" , "02",  "03" , "04" , "05" , "06" , "07",  "08",  "09" , "10" ,"11", "12")
-años<-seq(2010,2017,1)
-años<-c(2016)
+a??os<-seq(2010,2017,1)
 #ejemplo de liga a csv ecobici
 #https://www.ecobici.cdmx.gob.mx/sites/default/files/data/usages/2017-01.csv
 
 lista<-c()
-for (a in años)
+for (a in a??os)
 {
   for(m in meses)
   {
@@ -159,7 +175,7 @@ for (a in años)
     lista<-c(lista,s)
   }}
 
-#quitar los años / meses que no existen (esto se puede quitar con un try catch )
+#quitar los a??os / meses que no existen (esto se puede quitar con ~ try catch )
 #lista<-lista[-c(1,87:96)]
 
 l<-1
@@ -172,7 +188,6 @@ for (l in 2:length(lista))
   dat<-rbind(dat,mydat)
   print(l)
 }
-
 fwrite(dat,file="ecobici.csv",col.names=FALSE)
 
 
@@ -189,26 +204,26 @@ for( i in 1:dim(data)[2])
   print(names(data)[i])
   print(class(data[,i]))
 }
-#data<-data[,-1]
 
-###Separar por años para facilitar el handling en memoria
-años<-seq(2010,2017,1)
-year<-paste(años)
-y2<-substr(data$Fecha_Retiro,1,4)
-for (n in year)
-{
-  ind<-which(y2==n)
-  dat<-data[ind,]
-  fwrite(dat,file=paste("ecobici",n,".csv",sep=""),col.names=FALSE)
-  print(n)
-}
+###Separar por a??os para facilitar el handling en memoria
+# a??os<-seq(2010,2017,1)
+# year<-paste(a??os)
+# y2<-substr(data$Fecha_Retiro,1,4)
+# for (n in year)
+# {
+#   ind<-which(y2==n)
+#   dat<-data[ind,]
+#   fwrite(dat,file=paste("ecobici",n,".csv",sep=""),col.names=FALSE)
+#   print(n)
+# }
+#
 
-
-#Leer y manipular cada año de datos.
+#Leer y manipular cada a??o de datos.
 rm(list=ls(all=TRUE))
-años<-seq(2010,2017,1)
-años<-seq(2012,2017,1)
-year<-paste(años)
+a??os<-seq(2010,2017,1)
+a??os<-seq(2012,2017,1)
+year<-paste(a??os)
+
 n<-year[1]
 
 for (n in year)
@@ -216,82 +231,164 @@ for (n in year)
   data<-fread(file=paste("ecobici",n,".csv",sep=""))
   data<-as.data.frame(data)
   names(data)<-c("Genero_Usuario","Edad_Usuario","Bici","Ciclo_Estacion_Retiro","Fecha_Retiro","Hora_Retiro","Ciclo_Estacion_Arribo","Fecha_Arribo","Hora_Arribo")
-  #transformar los datos de tiempo
-  
-  
-  Fecha_Retiro<-mclapply(Fecha_Retiro,function(x) gsub("/","-",x,fixed=TRUE))
-  Fecha_Retiro<-mclapply(Fecha_Retiro,function(x) strsplit(Fecha_Retiro,"-",fixed=TRUE)))
-                                             
 
-    
-  dates<-as.Date(data$Fecha_Retiro)
-  year<-substr(data$Fecha_Retiro,1,4)
-  month<-substr(data$Fecha_Retiro,4,5)
-  Hora_T<-as.numeric(gsub(":","",substr(data$Hora_Retiro,1,2),fixed=TRUE))
-  
-  data<-cbind(data,year,month,Hora_T)
-  names(data)<-c(names(data)[1:(dim(data)[2]-3)],"Year","Mes","Hora_T")
-  
-  rm(year,month,Hora_T)
-  
-  #cual era el dia de la semana
-  weekdays<-weekdays(as.Date(data$Fecha_Retiro))
-  #duracion del viaje
-  #difftime(Hora_Arribo[1],Hora_Retiro[1])
-  aux<-seq(1:dim(data)[1])
-  
-  aux1<-unlist(lapply(seq(1:dim(data)[1]),function(x) substr(data$Hora_Retiro[x],1,8)))
-  aux2<-unlist(lapply(seq(1:dim(data)[1]),function(x) substr(data$Hora_Arribo[x],1,8)))
-  aux3<-strptime(aux1,format="%H:%M:%S",tz="")
-  aux4<-strptime(aux2,format="%H:%M:%S",tz="")
-  
-  interval<-sapply(aux,function(x) difftime(aux4[x],aux3[x]))
-  
-  data<-cbind(data,interval)
-  data<-cbind(data,weekdays)
-  data$weekdays<-as.character(data$weekdays)
-  names(data)<-c(names(data)[1:(dim(data)[2]-2)],"duracion","dia_semana")
-  dia<-substr(data$Fecha_Retiro,9,10)
-  data<-cbind(data,dia)
-  names(data)[length(names(data))]<-"dia_mes"
-  
-  rm(dia,weekdays,interval,aux3,aux4,aux2,aux1)
-  
-  #viajes que regresaron a la misma estación
-  aux2<-sapply(aux,function(x) data$Ciclo_Estacion_Arribo[x]==data$Ciclo_Estacion_Retiro[x])
-  data<-cbind(data,aux2)
-  names(data)[dim(data)[2]]<-"Misma_Estacion"
-  
-  rm(aux2)
-  #hay un tema de que algunas entradas tienen hora de entrega 0 que != a 00:00:00. Esto puede significar dos cosas, no entregaron la bici o la entregaron justo a la hora cero. Esto es importante saberlo
-  
-  # numero estaciones con arribo o destino no identificados
-  #452 es el numero total de estaciones
-  ind1<-which(data$Ciclo_Estacion_Arribo>452)
-  ind2<-which(data$Ciclo_Estacion_Retiro>452)
-  
-  
-  #intersect(ind1,ind2)
-  #length(ind1)+length(ind2)-length(intersect(ind1,ind2))
-  #quitar las entradas con esta estacion desconocida
-  aux<-unique(sort(c(ind1,ind2)))
-  length(aux)
-  if(length(aux)!=0){
-    data<-data[-aux,]}
-  
-  #calcular distancia del viaje
-  distancias<-fread("https://raw.githubusercontent.com/DianaSifuentesMunch/Ecobici/Diana/distancias.csv")
-  distancias<-as.matrix(distancias)
-  distance<-sapply(seq(1:dim(data)[1]),function(x) distancias[data$Ciclo_Estacion_Retiro[x],data$Ciclo_Estacion_Arribo[x]])
-  #agregarl la columna a nuestro database
-  data<-cbind(data,distance)
-  names(data)[dim(data)[2]]<-"distancia"
-  
-  fwrite(data,file=paste("ecobici",n,"_C",".csv",sep=""),row.names=FALSE,col.names=TRUE)
-  
-  rm(distance,distancias,aux,ind1,ind2,data)
-  gc()
+
+
+#transformar los datos de fecha de Retiro de la bici pq son distintos algunos
+  year<-rep(NA,dim(data)[1])
+  month<-year
+  day<-month
+  vector<-data$Fecha_Retiro
+
+  pos<-regexpr("/",vector)
+  auxpos<-which(pos != -1 )
+  upos<-setdiff(unique(pos),-1)
+  split<-strsplit(vector[auxpos],"/",fixed=TRUE)
+  split2<-strsplit(vector[-auxpos],"-",fixed=TRUE)
+
+  if(upos == 3)
+{
+  if(!is.null(auxpos))
+  {
+  auxseq<-1:length(split)
+  day[auxpos]<-as.numeric(unlist(lapply(auxseq,function(x) split[[x]][1])))
+  month[auxpos]<-as.numeric(unlist(lapply(auxseq,function(x) split[[x]][2])))
+  year[auxpos]<-as.numeric(unlist(lapply(auxseq,function(x) split[[x]][3])))
+  }
+  if(!is.null(day[-auxpos]))
+  {
+  auxseq<-1:length(split2)
+  day[-auxpos]<-as.numeric(unlist(lapply(auxseq,function(x) split2[[x]][3])))
+  month[-auxpos]<-as.numeric(unlist(lapply(auxseq,function(x) split2[[x]][2])))
+  year[-auxpos]<-as.numeric(unlist(lapply(auxseq,function(x) split2[[x]][1])))
+  }
+}else{
+  auxseq<-1:length(split)
+  day[auxpos]<-as.numeric(unlist(lapply(auxseq,function(x) split[[x]][3])))
+  month[auxpos]<-as.numeric(unlist(lapply(auxseq,function(x) split[[x]][2])))
+  year[auxpos]<-as.numeric(unlist(lapply(auxseq,function(x) split[[x]][1])))
+  auxseq<-1:length(split2)
+  day[-auxpos]<-as.numeric(unlist(lapply(auxseq,function(x) split2[[x]][1])))
+  month[-auxpos]<-as.numeric(unlist(lapply(auxseq,function(x) split2[[x]][2])))
+  year[-auxpos]<-as.numeric(unlist(lapply(auxseq,function(x) split2[[x]][3])))
+  print("POSIBLE ERROR FECHAS")
 }
+
+newdates<-as.Date(paste(day,"-",month,"-",year,sep=""),format="%d-%m-%Y")
+data$Fecha_Retiro<-newdates
+
+#cual era el dia de la semana
+weekdays<-weekdays(newdates)
+#Hora Truncada
+Hora_T<-as.numeric(gsub(":","",substr(data$Hora_Retiro,1,2),fixed=TRUE))
+#agregar datos
+data<-cbind(data,year,month,day,Hora_T,weekdays)
+names(data)<-c(names(data)[1:(dim(data)[2]-5)],"Year","Mes","dia_mes","Hora_T","dia_semana")
+rm(auxpos,day,Hora_T,month,newdates,pos,split,split2,upos,vector,weekdays,year)
+
+
+#Lo mismo para la fecha de arribo
+year<-rep(NA,dim(data)[1])
+month<-year
+day<-month
+vector<-data$Fecha_Arribo
+pos<-regexpr("/",vector)
+auxpos<-which(pos != -1 )
+upos<-setdiff(unique(pos),-1)
+split<-strsplit(vector[auxpos],"/",fixed=TRUE)
+split2<-strsplit(vector[-auxpos],"-",fixed=TRUE)
+if(upos == 3)
+{
+  if(!is.null(auxpos))
+  {
+    auxseq<-1:length(split)
+    day[auxpos]<-as.numeric(unlist(lapply(auxseq,function(x) split[[x]][1])))
+    month[auxpos]<-as.numeric(unlist(lapply(auxseq,function(x) split[[x]][2])))
+    year[auxpos]<-as.numeric(unlist(lapply(auxseq,function(x) split[[x]][3])))
+  }
+  if(!is.null(day[-auxpos]))
+  {
+    auxseq<-1:length(split2)
+    day[-auxpos]<-as.numeric(unlist(lapply(auxseq,function(x) split2[[x]][3])))
+    month[-auxpos]<-as.numeric(unlist(lapply(auxseq,function(x) split2[[x]][2])))
+    year[-auxpos]<-as.numeric(unlist(lapply(auxseq,function(x) split2[[x]][1])))
+  }
+}else{
+  auxseq<-1:length(split)
+  day[auxpos]<-as.numeric(unlist(lapply(auxseq,function(x) split[[x]][3])))
+  month[auxpos]<-as.numeric(unlist(lapply(auxseq,function(x) split[[x]][2])))
+  year[auxpos]<-as.numeric(unlist(lapply(auxseq,function(x) split[[x]][1])))
+  auxseq<-1:length(split2)
+  day[-auxpos]<-as.numeric(unlist(lapply(auxseq,function(x) split2[[x]][1])))
+  month[-auxpos]<-as.numeric(unlist(lapply(auxseq,function(x) split2[[x]][2])))
+  year[-auxpos]<-as.numeric(unlist(lapply(auxseq,function(x) split2[[x]][3])))
+  print("POSIBLE ERROR FECHAS")
+}
+newdates<-as.Date(paste(day,"-",month,"-",year,sep=""),format="%d-%m-%Y")
+data$Fecha_Arribo<-newdates
+rm(auxseq,year,month,day,vector,auxpos,newdates,pos,split,split2,upos,vector,wee)
+
+#duracion del viaje (Las horas tienen algunas un punto entonces hay que unificar los datos)
+vector<-data$Hora_Retiro
+split<-strsplit(vector,".",fixed=TRUE)
+horas<-unlist(lapply(seq(1:length(split)),function(x) split[[x]][1]))
+
+vector<-data$Hora_Arribo
+split<-strsplit(vector,".",fixed=TRUE)
+horas2<-unlist(lapply(seq(1:length(split)),function(x) split[[x]][1]))
+
+aux<-1:length(horas)
+#estas ser??an las fechas con hora completas, se necesita para calcular r??pidamente la duraci??n del viaje
+horas.1<-fastPOSIXct(paste(data$Fecha_Retiro,horas),tz="UTC")
+horas.2<-fastPOSIXct(paste(data$Fecha_Arribo,horas2),tz="UTC")
+
+interval<-(horas.2-horas.1)/60
+interval<-difftime(horas.2,horas.1)
+
+data<-cbind(data,interval,horas.1,horas.2)
+names(data)<-c(names(data)[1:(dim(data)[2]-3)],"duracion","FRetiro_Comp","FArribo_Comp")
+
+
+rm(aux,horas,horas.1,horas.2,horas2,split,vector)
+
+#viajes que regresaron a la misma estaci??n
+aux2<-data$Ciclo_Estacion_Arribo==data$Ciclo_Estacion_Retiro
+data<-cbind(data,aux2)
+names(data)[dim(data)[2]]<-"Misma_Estacion"
+
+rm(aux2)
+
+#hay un tema de que algunas entradas tienen hora de entrega 0 que != a 00:00:00. Esto puede significar dos cosas, no entregaron la bici o la entregaron justo a la hora cero. Esto es importante saberlo
+# numero estaciones con arribo o destino no identificados
+#452 es el numero total de estaciones
+ind1<-which(data$Ciclo_Estacion_Arribo>452)
+ind2<-which(data$Ciclo_Estacion_Retiro>452)
+#intersect(ind1,ind2)
+#length(ind1)+length(ind2)-length(intersect(ind1,ind2))
+#quitar las entradas con esta estacion desconocida
+aux<-unique(sort(c(ind1,ind2)))
+length(aux)
+if(length(aux)!=0){
+  data<-data[-aux,]}
+
+
+
+#calcular distancia del viaje
+distancias<-fread("distancias.csv")
+distancias<-as.matrix(distancias)
+
+distance<-unlist(lapply(seq(1:dim(data)[1]),function(x) distancias[data$Ciclo_Estacion_Retiro[x],data$Ciclo_Estacion_Arribo[x]]))
+#agregarl la columna a nuestro database
+
+data<-cbind(data,distance)
+names(data)[dim(data)[2]]<-"distancia"
+
+fwrite(data,file=paste("ecobici",n,"_C",".csv",sep=""),row.names=FALSE,col.names=TRUE)
+
+rm(distance,distancias,aux,ind1,ind2,data)
+gc()
+}
+
 
 
 
